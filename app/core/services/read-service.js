6b7f91ed6d6e52db2e78b17ca17ed674ce8cf925
TsinjoAndrianarivony <tsinjo.andrianarivony@gmail.com>
2024-02-11 18:55:45 +0300

    [M1MEAN-28][SaveAppointment] - update crud service

--- app/core/services/read-service.js
+++ app/core/services/read-service.js
@@ -16,26 +16,14 @@ export class ReadService {
     this.ignoredFields.forEach( key => this.projection[key] = 0);
   }
 
-  /**
-   * Retrieves all entities with optional sorting.
-   *
-   * @param {Object} sort - The sorting options.
-   * @returns {Promise} A promise that resolves to the array of entities.
-   */
-  async findAll(sort = {}) {
-    let query = this.Model.find({}, this.projection);
+  async findAll({sort, search} = {
+    sort: {},
+    search: {}
+  }) {
+    let query = this.Model.find(search, this.projection);
     return await this.sortQuery(query, sort);
   }
 
-
-  /**
-   * Sorts the given query based on the provided sorting options.
-   *
-   * @param {Object} query - The MongoDB query.
-   * @param {Object} sort - The sorting options.
-   * @returns {Object} The sorted MongoDB query.
-   * @private
-   */
   sortQuery(query, sort) {
     const { column, method } = sort;
     if (column && method) {
@@ -47,17 +35,12 @@ export class ReadService {
     return query;
   }
 
-  /**
-   * Retrieves paginated entities with optional sorting.
-   *
-   * @param {number} page - The page number.
-   * @param {number} offset - The number of items per page.
-   * @param {Object} sort - The sorting options.
-   * @returns {Promise} A promise that resolves to the paginated result.
-   */
-  async findAllPaginated(page = 1, offset = 10, sort = {}) {
+  async findAllPaginated(page = 1, offset = 10, {sort, search} = {
+    sort: {},
+    search: {}
+  }) {
     if (page < 0) page = 1;
-    const query = this.Model.find({}, this.projection)
+    const query = this.Model.find(search, this.projection)
       .limit(offset)
       .skip((page - 1) * offset)
       .sort({ createdAt: -1 });
@@ -72,12 +55,6 @@ export class ReadService {
     };
   }
 
-  /**
-   * Retrieves an entity by ID.
-   *
-   * @param {string} id - The ID of the entity to retrieve.
-   * @returns {Promise} A promise that resolves to the retrieved entity.
-   */
   async findById(id) {
     return await this.Model.findById(id, this.projection);
   }
