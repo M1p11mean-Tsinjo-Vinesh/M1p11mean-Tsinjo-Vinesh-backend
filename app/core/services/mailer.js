3aa110a780892617c4ba6588ef2d3ae2b4e10159
TsinjoAndrianarivony <tsinjo.andrianarivony@gmail.com>
2024-02-15 17:30:43 +0300

    [Mailer] - update mailer

--- app/core/services/mailer.js
+++ app/core/services/mailer.js
@@ -1,48 +1,69 @@
-import { createTransport } from "nodemailer";
+import dotenv from "dotenv";
+import axios from "axios";
 
-// Initialize variables for nodemailer transporter and mail configuration
-let transporter = null;
-let MAIL = null;
+// dot env support
+dotenv.config();
 
-/**
- * Initializes the nodemailer transporter with the provided mail configuration.
- * If the transporter is not already created, it uses the environment variables for mail and password.
- */
-const init = () => {
-  if (!transporter) {
-    MAIL = process.env.MAIL;
-    const mailConfig = {
-      service: "Gmail",
-      host: "smtp.gmail.com",
-      port: 465,
-      secure: true,
-      auth: {
-        user: MAIL,
-        pass: process.env.MAIL_PASSWORD
+export class Mailer {
+
+  SENDER = process.env.MAIL_SENDER;
+  API_KEY = process.env.MAIL_API_KEY;
+  URL = process.env.MAIL_API_URL;
+  requestConfig;
+
+  constructor() {
+    this.requestConfig = {
+      headers: {
+        "X-ElasticEmail-ApiKey": this.API_KEY
       }
-    };
-    transporter = createTransport(mailConfig);
+    }
   }
-}
 
-/**
- * Object containing methods related to email functionality.
- */
-export const mailer = {
-  /**
-   * Sends an email using the configured transporter.
-   *
-   * @param {Object} data - The email data including 'to', 'subject', 'text', 'html', etc.
-   * @returns {Promise} A promise that resolves when the email is sent successfully.
-   */
-  sendMail: async data => {
-    // Initialize the transporter if not already done
-    init();
-
-    // Use the transporter to send the email
-    return await transporter.sendMail({
-      from: MAIL,
-      ...data
-    });
+  async send({
+    dates, // list of dates to send the email
+    ...rest
+  }) {
+    const promises = [];
+    for (let date of dates) {
+      let delay = new Date(date).getTime() - new Date().getTime();
+      delay = delay < 0 ? 0 : delay / 60000;
+      const sent = axios.post(
+        this.URL,
+        this.createApiBody({delay, ...rest}),
+        this.requestConfig
+      )
+      promises.push(sent);
+    }
+    return await Promise.all(promises);
   }
+
+
+  createApiBody({
+      recipients, // string[],
+      subject,
+      contentType = "HTML", // HTML or Plain Text of body
+      content, // text
+      delay = 0
+  }) {
+    return {
+      Recipients: recipients.map(Email => ({Email})),
+        Content: {
+      Body: [
+        {
+          ContentType: contentType,
+          Content: content
+        }
+      ],
+        From: this.SENDER,
+        Subject: subject
+    },
+      Options: {
+        TimeOffset: parseInt(delay.toString())
+      }
+    }
+  }
+
+
+
+
 }
