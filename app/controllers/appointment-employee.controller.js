e35ae1069cb429a815743c039e502d80ae771cad
TsinjoAndrianarivony <tsinjo.andrianarivony@gmail.com>
2024-02-16 10:06:01 +0300

    [Appointment] - mark element as done

--- app/controllers/appointment-employee.controller.js
+++ app/controllers/appointment-employee.controller.js
@@ -1,11 +1,23 @@
 import {ReadController} from "#core/controllers/read-controller.js";
 import {CrudService} from "#core/services/crud-service.js";
 import {AppointmentDetailsModel} from "#models/appointment.model.js";
+import {AppointmentDetailsService} from "#services/appointment-details.service.js";
+import {respond} from "#core/util.js";
 
 export class AppointmentEmployeeController extends ReadController {
 
   constructor() {
-    super(new CrudService(AppointmentDetailsModel), ["client._id", "service._id", "startDate", "status"]);
+    super(new AppointmentDetailsService(), ["client._id", "service._id", "startDate", "status"]);
+  }
+
+  async markAppointmentElementDone(req, res, next) {
+    try {
+      await this.service.markAsDone(req.params.id, req.user._id);
+      respond(res, 204);
+    }
+    catch (e) {
+      next(e)
+    }
   }
 
   createFilterOptions(req) {
@@ -14,6 +26,11 @@ export class AppointmentEmployeeController extends ReadController {
       ...filterOptions,
       "employee._id": req.user._id
     }
+  }
+
+  buildRouter() {
+    return super.buildRouter()
+      .register("put", "/:id/done", this.markAppointmentElementDone.bind(this));
   }
 
 }
--- app/services/appointment-details.service.js
+++ app/services/appointment-details.service.js
@@ -1,10 +1,32 @@
 import {CrudService} from "#core/services/crud-service.js";
 import {AppointmentDetailsModel} from "#models/appointment.model.js";
+import {BadRequest} from "#core/util.js";
 
 export class AppointmentDetailsService extends CrudService {
 
   constructor() {
     super(AppointmentDetailsModel);
+  }
+
+  /**
+   * Marks an element of an appointment as done.
+   * Performed by the assigned employee.
+   * @param appointmentElementsId
+   * @param employeeId
+   * @returns {Promise<*>}
+   */
+  async markAsDone(appointmentElementsId, employeeId) {
+    const element = await this.findOne({
+      _id: appointmentElementsId,
+      "employee._id": employeeId
+    });
+    if (element) {
+      if (element.status < 10) {
+        throw BadRequest("Opération invalide: Tache non validé");
+      }
+      return await this.update(appointmentElementsId, {status: 30});
+    }
+    throw BadRequest("Rendez-vous inexistant");
   }
 
   async create(data) {
