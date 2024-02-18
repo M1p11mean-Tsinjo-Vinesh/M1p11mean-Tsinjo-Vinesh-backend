90f3229715ae2d547bae31c51d87ddc020ef1327
TsinjoAndrianarivony <tsinjo.andrianarivony@gmail.com>
2024-02-18 20:46:16 +0300

    [ExpenseCRUD] - add time-key to expense

--- app/routes/expense.route.js
+++ app/routes/expense.route.js
@@ -1,9 +1,8 @@
+import {ExpenseService} from "#services/expense.service.js";
 import {CrudController} from "#core/controllers/crud-controller.js";
-import {CrudService} from "#core/services/crud-service.js";
-import {ExpenseModel} from "#models/expense.model.js";
 
 const expenseController = new CrudController(
-  new CrudService(ExpenseModel),
+  new ExpenseService(),
   ["year", "month", "type"]
 );
 
