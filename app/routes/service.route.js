ce09eb195b57433416ef004105a1f8bf1768ea22
TsinjoAndrianarivony <tsinjo.andrianarivony@gmail.com>
2024-02-29 06:47:11 +0300

    [Offers] - create service for created offer

--- app/routes/service.route.js
+++ app/routes/service.route.js
@@ -1,6 +1,4 @@
 import {CrudController} from "#core/controllers/crud-controller.js";
-import {CrudService} from "#core/services/crud-service.js";
-import {ServiceModel} from "#models/service.model.js";
 import {ServicesService} from "#services/services.service.js";
 
 export const servicesService = new ServicesService();
--- app/services/offer.service.js
+++ app/services/offer.service.js
@@ -11,11 +11,62 @@ export class OfferService extends CrudService {
   }
 
   async create(data) {
-    return await super.create(data);
+    const result = await super.create(data);
+    this.createServiceFrom(result).catch(console.log);
+    return result;
+  }
+
+  async createServiceFrom(offer) {
+    let service = await this.createServiceDataFromOffer(offer._id);
+    service = await this.serviceServices.create(service);
+    await OfferModel.findByIdAndUpdate(offer._id, {
+      serviceId: service._id
+    });
   }
 
   async update(id, data) {
-    return await super.update(id, data);
+    const result = await super.update(id, data);
+    this.updateServiceFrom(result).catch(console.log);
+    return result;
+  }
+
+  async updateServiceFrom(offer) {
+    const service = await this.createServiceDataFromOffer(offer._id);
+    await this.serviceServices.update(offer.serviceId, service);
+  }
+
+  async createServiceDataFromOffer(offerId) {
+    const {
+      name,
+      startDate,
+      endDate,
+      services,
+      pictureUrls
+    } = (await this.findById(offerId))._doc;
+    const realValue = services.reduce((prev, curr) => prev + curr.price, 0);
+    const [
+      duration,
+      commission,
+      discount
+    ] = [
+      services.reduce((prev, curr) => prev + curr.duration,  0),
+      services.reduce((prev, curr) => prev + (curr.price * curr.commission), 0) /realValue,
+      services.reduce((prev, curr) => prev + (curr.price * curr.discount), 0) / realValue
+    ];
+    const price = realValue - (realValue * discount);
+    return {
+      name,
+      pictureUrls,
+      duration,
+      price,
+      commission,
+      startDate,
+      endDate,
+      discountInformation: {
+        discountValue: discount,
+        value: realValue
+      }
+    }
   }
 
   async remove(id) {
